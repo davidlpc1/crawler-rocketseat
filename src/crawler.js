c1ebd77e6f68b509edcc598ccfca4b081ec6bdf7
davidlpc1 <davipadro76@gmail.com>
2020-11-19 17:18:52 -0300

    Tudo pronto!

---
+++ src/crawler.js
@@ -0,0 +1,46 @@
+const puppeteer = require('puppeteer-core');
+const os = require('os');
+const getVideosTime = require('./getVideoTime');
+
+const executablePaths = {
+    'linux':'/user/bin/google-chrome',
+    'darwin':'/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
+    'win32':'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe'
+}
+
+const platform = os.platform();
+
+(async () => {
+  const browser = await puppeteer.launch({
+    headless: true,
+    executablePath: executablePaths[platform]
+  });
+
+  const page = await browser.newPage();
+
+  const cdDiegao = await getVideosTime(page, 'https://www.youtube.com/playlist?list=PL85ITvJ7FLohhULgUFkYBf2xcXCG6yfVV')
+  const cdMaykao = await getVideosTime(page, 'https://www.youtube.com/playlist?list=PL85ITvJ7FLoifcDIBeuuAhh4_799RZaSc')
+  
+
+  let hours = cdDiegao[0] + cdMaykao[0];
+  let minutes = cdDiegao[1] + cdMaykao[1];
+  let seconds = cdDiegao[2] + cdMaykao[2];
+
+  // formatar da maneira correta
+
+  const minutesFromSeconds = Math.floor(seconds / 60);
+  
+  seconds = seconds % 60;
+  minutes += minutesFromSeconds;
+
+  const hoursFromMinutes = Math.floor(minutes / 60);
+
+  minutes = minutes % 60;
+  hours += hoursFromMinutes;
+
+  console.log(
+    `Produzimos ${hours}:${minutes}:${seconds} de conteúdo para o Code/Drops`
+  );
+  
+  await browser.close();
+})();
